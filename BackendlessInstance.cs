31a936bdcb0f80c718608c88de1cf76e55bbf1d9
wjpldevs <wjpldevs@gmail.com>
2022-12-14 22:11:34 -0600

    Add project files.

---
+++ BackendlessInstance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackendlessAPI;
+
+namespace backendless_console_app
+{
+    public class BackendlessInstance
+    {
+        public BackendlessInstance() {
+            string appID = ""; // ingrese su app ID aquí
+            string apiKey = ""; // ingrese su api key de .NET
+            Backendless.InitApp(appID, apiKey);
+        }
+
+        public BackendlessUser AgregarUsuario (string name, string email, string pass) { 
+            BackendlessUser newUser = new BackendlessUser();
+            newUser.SetProperty("login", name);
+            newUser.SetProperty("email", email);
+            newUser.Password = pass;
+            return Backendless.UserService.Register(newUser);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backendless_console_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre del usuario: ");
+            string nombreUsuario = Console.ReadLine();
+
+            Console.WriteLine("Correo del usuario: ");
+            string emailUsuario = Console.ReadLine();
+
+            Console.WriteLine("Contrasenia del usuario: ");
+            string passUsuario = Console.ReadLine();
+
+            BackendlessInstance backendless = new BackendlessInstance();
+
+            var usuario = backendless.AgregarUsuario(nombreUsuario, emailUsuario, passUsuario);
+
+            Console.WriteLine("Usuario registrado {0}", usuario.Properties["objectId"]);
+
+            Console.ReadKey();
+        }
+    }
+}
